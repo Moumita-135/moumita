6d771e0741caee1f4520f28782d88be8c6b1695e
Moumita Mondal <mondalmoumita535@gmail.com>
2021-12-15 14:53:36 +0530

    Modify Commit

--- rolebase/settings.py
+++ rolebase/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5g&%hc@-j9wd)4rr8$zy_vgpwgpc$chb)159@-ztc9tn5=xei(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -77,23 +77,23 @@ WSGI_APPLICATION = 'rolebase.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'djongo',
-        'NAME': 'TDF',
-    }
-}
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'djongo',
 #         'NAME': 'TDF',
-#         'CLIENT': {
-#             'host': 'mongodb+srv://moumita:FcDnEXYlIm7TYWv1@cluster0.kvwva.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-#         },
-#         'ENFORCE_SCHEMA': False,
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'TDF',
+        'CLIENT': {
+            'host': 'mongodb+srv://moumita:FcDnEXYlIm7TYWv1@cluster0.kvwva.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+        },
+        'ENFORCE_SCHEMA': False,
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
